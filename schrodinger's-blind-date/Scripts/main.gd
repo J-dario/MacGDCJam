3324b2f11fac361cf425dad4821fee01e6740047
Javier Dario <dariojavier485@gmail.com>
2026-03-26 17:09:38 -0400

    main menu, part of intro, choice start anim

--- schrodinger's-blind-date/Scripts/main.gd
+++ schrodinger's-blind-date/Scripts/main.gd
@@ -1,17 +1,46 @@
 extends Node3D
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
-@onready var voice: AudioStreamPlayer2D = $Voice
-@onready var subtitles: RichTextLabel = $Subtitles
+@onready var voice: AudioStreamPlayer = $Voice
+@onready var ui_anim: AnimationPlayer = $UIAnim
+@onready var subtitles: RichTextLabel = $CanvasLayer/Subtitles
+@onready var next_arrow: Sprite2D = $CanvasLayer/Subtitles/NextArrow
 
-func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("ui_accept"):
-		animation_player.play("Intro")
+var gameStart = false
+var canContinue = false
 
-func nextVoice(audio: AudioStream, text: String, color: Color) -> void:
+func _input(event: InputEvent) -> void:
+	if Input.is_action_just_pressed("LeftClick"):
+		animation_player.play()
+
+func nextVoice(audio: AudioStream, text: String, color: Color, volume: float = 0.0) -> void:
+	canContinue = false
+	next_arrow.hide()
+	
 	voice.stream = audio
+	voice.volume_db = volume
 	voice.play()
+	subtitles.show()
 	subtitles.clear()
 	subtitles.push_color(color)
 	subtitles.add_text(text)
 	subtitles.pop()
+
+func endText():
+	subtitles.hide()
+
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	if anim_name == "FadeFromBlack" and !gameStart:
+		animation_player.play("Intro")
+		gameStart = true
+		
+		animation_player.seek(25, true)
+
+func _on_voice_finished() -> void:
+	animation_player.pause()
+	canContinue = true
+	next_arrow.show()
+	print(animation_player.current_animation_position)
+
+func startChoice():
+	ui_anim.play("CloseEyes")
--- schrodinger's-blind-date/Scripts/main_menu.gd
+++ schrodinger's-blind-date/Scripts/main_menu.gd
@@ -2,9 +2,11 @@ extends Node3D
 
 const MAIN = preload("uid://o4p3bjwyo7r2")
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var audio_stream_player_2: AudioStreamPlayer = $AudioStreamPlayer2
 
 func _on_button_pressed() -> void:
 	animation_player.play("FadeIn")
+	audio_stream_player_2.play()
 
 
 func _on_animation_player_animation_finished(anim_name: StringName) -> void:
